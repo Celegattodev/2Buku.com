c5833c70277debc533c2a49d7a35ab672b687449
Murilo <murilo.celegatto@gmail.com>
2024-09-11 19:49:04 -0300

    Page AboutUs
    Nova Navbar; Nova Landing Page; Começo do desenvolvimento da página de explicação e dúvidas frequentes;

---
+++ assets/js/aboutUs/aboutUs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+});
