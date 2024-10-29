15be56f003f00d8323b84da9490f1f324b7d8c4c
fernandes <enzo.dantas@hotmail.com>
2024-10-29 14:48:11 -0300

    Atualizações no server

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Configura o diretório de arquivos estáticos
-app.use(express.static(path.join(__dirname, "assets")));
+app.use('/assets', express.static(path.join(__dirname, "assets")));
 
 // Configura o middleware para parsing de corpo das requisições
 app.use(express.urlencoded({ extended: true }));
