421ecc38eaa62271b4e9d41873b0fbcc392da8b9
Enzo Fernandes <enzo.dantas@hotmail.com>
2024-11-08 22:35:04 -0300

    banco

--- banco de dados/buku_db.sql
+++ banco de dados/buku_db.sql
@@ -1,33 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 07/11/2024 às 23:10
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `buku_db`
---
-
 CREATE DATABASE buku_db;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `favoritos`
---
 
 CREATE TABLE `favoritos` (
   `id` int(11) NOT NULL,
@@ -38,11 +15,6 @@ CREATE TABLE `favoritos` (
   `google_books_id` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `livros`
---
 
 CREATE TABLE `livros` (
   `id` int(11) NOT NULL,
@@ -54,11 +26,6 @@ CREATE TABLE `livros` (
   `data_adicao` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `livro_imagens`
---
 
 CREATE TABLE `livro_imagens` (
   `id` int(11) NOT NULL,
@@ -67,11 +34,6 @@ CREATE TABLE `livro_imagens` (
   `imagem_url` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `trocas`
---
 
 CREATE TABLE `trocas` (
   `id` int(11) NOT NULL,
@@ -86,11 +48,6 @@ CREATE TABLE `trocas` (
   `status` enum('Pendente','Aceito','Recusado') DEFAULT 'Pendente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -106,114 +63,69 @@ CREATE TABLE `users` (
   `photo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices de tabela `favoritos`
---
 ALTER TABLE `favoritos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Índices de tabela `livros`
---
+
 ALTER TABLE `livros`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Índices de tabela `livro_imagens`
---
 ALTER TABLE `livro_imagens`
   ADD PRIMARY KEY (`id`),
   ADD KEY `livro_id` (`livro_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Índices de tabela `trocas`
---
+
 ALTER TABLE `trocas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_solicitante_id` (`usuario_solicitante_id`),
   ADD KEY `usuario_recebedor_id` (`usuario_recebedor_id`),
   ADD KEY `livro_solicitante_id` (`livro_solicitante_id`);
 
---
--- Índices de tabela `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `favoritos`
---
 ALTER TABLE `favoritos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
---
--- AUTO_INCREMENT de tabela `livros`
---
+
 ALTER TABLE `livros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 
---
--- AUTO_INCREMENT de tabela `livro_imagens`
---
+
 ALTER TABLE `livro_imagens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=154;
 
---
--- AUTO_INCREMENT de tabela `trocas`
---
+
 ALTER TABLE `trocas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
---
--- AUTO_INCREMENT de tabela `users`
---
+
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
---
--- Restrições para tabelas despejadas
---
 
---
--- Restrições para tabelas `favoritos`
---
 ALTER TABLE `favoritos`
   ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
---
--- Restrições para tabelas `livros`
---
+
 ALTER TABLE `livros`
   ADD CONSTRAINT `livros_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
---
--- Restrições para tabelas `livro_imagens`
---
+
 ALTER TABLE `livro_imagens`
   ADD CONSTRAINT `livro_imagens_ibfk_1` FOREIGN KEY (`livro_id`) REFERENCES `livros` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `livro_imagens_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
---
--- Restrições para tabelas `trocas`
---
+
 ALTER TABLE `trocas`
   ADD CONSTRAINT `trocas_ibfk_1` FOREIGN KEY (`usuario_solicitante_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `trocas_ibfk_2` FOREIGN KEY (`usuario_recebedor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `trocas_ibfk_3` FOREIGN KEY (`livro_solicitante_id`) REFERENCES `livros` (`id`) ON DELETE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
